e449e03da7fb07d7edda97491c6696d0e5fca49d
hacktivities2025-ctrl <hacktivities.2025@gmail.com>
2025-11-30 10:57:49 +0400

    Update info-data-table.tsx

--- src/components/app/admin/info-data-table.tsx
+++ src/components/app/admin/info-data-table.tsx
@@ -14,6 +14,7 @@ import {
   VisibilityState,
 } from '@tanstack/react-table';
 
+
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
